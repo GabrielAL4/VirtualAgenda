b511aca66148bb79e055a1de5ac251064b3dbbc0
Gabriel Albuquerque <gabriel.albuquerque2001@gmail.com>
2022-01-31 10:52:14 -0300

    Tabela de eventos criada e filtro de usuários, data e nome

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+class DisplayHour(admin.ModelAdmin):
+    list_display = ('title', 'eventData', 'creationData')
+    list_filter = ('title', 'user','eventData')
+
+
+
+admin.site.register(Evento, DisplayHour)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    eventData = models.DateTimeField(verbose_name='Event Data')
+    creationData = models.DateTimeField(auto_now=True, verbose_name= 'Creation Data')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Evento'
+
+    def __str__(self):
+        return self.title
